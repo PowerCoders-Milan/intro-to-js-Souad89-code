9404eb3e59575a6c9cd303658894a2e2a24a84e3
Souad Allahham <souadallahham89@gmail.com>
2021-02-25 15:50:18 +0100

    I finished Ex-3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,11 @@
-// Example
-window.alert("Hello World!");
+let num1 = prompt("Please insert the first number!");
+let num2 = prompt("Please insert the second number!");
+if (num1 < num2) {
+    window.alert("The greater number of"+ num1+ "and"+ num2+ "is"+ num2); 
+  } 
+  else if (num1 > num2) {
+    window.alert("The greater number of"+ num1+ "and"+ num2+ "is"+ num1);
+  }
+  else {
+    window.alert("They are equal");
+  }

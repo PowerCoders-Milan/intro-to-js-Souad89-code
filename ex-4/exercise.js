f890efe698280cda2b0060fd01086ea2fc11f9d4
Souad Allahham <souadallahham89@gmail.com>
2021-02-25 17:15:06 +0100

    I finished Ex-4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,12 @@
-// Example
-window.alert("Hello World!");
+window.alert("Good Evening Souad!");
+window.alert("How was your day?");
+let response = prompt("Please insert your response!")
+if (response == "Good") {
+    window.alert("I'm happy for you Souad!");
+  } else if (response == "Bad") {
+    window.alert("That's ok, tomorrow it's a new day");
+  } else {
+    window.alert("no message");
+}
+
+

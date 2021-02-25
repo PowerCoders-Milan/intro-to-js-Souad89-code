439798c82dfa543e23e2fa9a836bced7b5173d42
Souad Allahham <souadallahham89@gmail.com>
2021-02-25 12:38:22 +0100

    I finished ex-1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,14 @@
-// Example
-window.alert("Hello World!");
+
+let firstName = "Souad";
+let sureName = "Allahham";
+let numberOfChildren = 2;
+let partnerName = "Lorenzo Polli";
+let address = "Milan";
+let jobTitle = "Business Analyst";
+console.log(firstName+ " " + sureName);
+console.log(numberOfChildren);
+console.log(partnerName);
+console.log(address);
+console.log(jobTitle);
+console.log(firstName+ " " + sureName+ " will be in "+ address+ " married to "+ partnerName+ " she will have "+ numberOfChildren+ " children "+ " and she will be working as "+ jobTitle);
+window.alert(firstName+ " " + sureName+ " will be in "+ address+ " married to "+ partnerName+ " she will have "+ numberOfChildren+ " children "+ " and she will be working as "+ jobTitle);

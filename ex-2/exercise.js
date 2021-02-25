86180c1afcf7e946a59826cdce0ef53cd420edc3
Souad Allahham <souadallahham89@gmail.com>
2021-02-25 15:00:41 +0100

    I finished ex-2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,6 @@
-// Example
-window.alert("Hello World!");
+
+let birthYear = prompt("Please insert your birth year!");
+let futureYeay = prompt("In which year do you want to know your age?");
+let requiredAge = futureYeay - birthYear;
+let requiredAge1 = futureYeay - birthYear + 1;
+window.alert(" I will be either "+ requiredAge+ " or "+ requiredAge1+ " in "+ futureYeay);
